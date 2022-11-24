bd77b7b67a1601dae3f449f786eef949cff99ed0
Andrei22501 <118145420+Andrei22501@users.noreply.github.com>
2022-11-24 12:09:41 +0300

    Test 1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private static final UserService userService = new UserServiceImpl();
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+        userService.saveUser("Andrei", "Solskii", (byte) 20);
+        userService.saveUser("Kirill", "Ivanov", (byte) 19);
+        userService.saveUser("Big", "Lebowskii", (byte) 53);
+        userService.saveUser("Ryan","Gosling", (byte) 43);
+        userService.removeUserById(1);
+        for (User user: userService.getAllUsers()){
+            System.out.println(user);
+        }
+
+
+
     }
 }
